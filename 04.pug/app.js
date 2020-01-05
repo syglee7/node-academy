d782aad012721780dfc8b5087961cf31b2b9c942
zena <syglee7@gmail.com>
2020-01-05 13:54:49 +0900

    2020

--- 04.pug/app.js
+++ 04.pug/app.js
@@ -25,6 +25,11 @@ app.get(["/pug", "/pug/:page"], (req, res) => {
     switch(page) {
         case "list":
             vals.title = "게시글 리스트 입니다.";
+            vals.lists = [
+                {id:1, title: '첫번째 글', writer: '관리자', wdate: '2020-01-03', rnum: 5},
+                {id:2, title: '두번째 글', writer: '이선영', wdate: '2020-01-04', rnum: 7},
+                {id:3, title: '세번째 글', writer: '현수호', wdate: '2020-01-05', rnum: 8},
+            ];
             res.render("list.pug", vals); 
             break;
         case "write":
